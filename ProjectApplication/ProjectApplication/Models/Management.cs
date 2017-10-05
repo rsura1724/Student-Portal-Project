48fc5a423a057efe5b88b274981c351cae8aa52e
rsura1724 <31663929+rsura1724@users.noreply.github.com>
2017-10-05 17:49:08 -0400

    Initial Commit
    Added the classes student and course

---
+++ ProjectApplication/ProjectApplication/Models/Management.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectApplication.Models
+{
+    public class Student
+    {
+        public int studentID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int courseID { get; set; }
+        public virtual Course course { get; set; }
+        public int deptID { get; set; }
+        public virtual Department department { get; set; }
+    }
+
+    public class Instructor
+    {
+    }
+
+    public class Course
+    {
+        public int courseID { get; set; }
+        public string courseName { get; set; }
+        public ICollection<Student> students { get; set; }
+        public ICollection<Instructor> instructors { get; set; }
+    }
+
+    public class Department
+    {
+    }
+}
